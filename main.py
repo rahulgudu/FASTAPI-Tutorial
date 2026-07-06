2a43e926085f670479009adc2c49a181bbd31cf0
Sushant Kumar Bishoi <117711778+rahulgudu@users.noreply.github.com>
2026-07-06 12:00:17 +0000

    query parameters

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ def home():
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     return {"item_id": item_id}
+
+# query params: /users?name=John
+@app.get("/users")
+def get_users(name: str = None): # query params are optional, so we can set default value to None
+    return {"name": name}
+
+@app.get("/products")
+def get_products(price: float = 20):
+    return {"price": price}
