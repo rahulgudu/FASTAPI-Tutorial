a366534a68634a7aa6668df76acebb99e2cf150e
Sushant Kumar Bishoi <117711778+rahulgudu@users.noreply.github.com>
2026-07-06 11:04:30 +0000

    add item retrieval endpoint to main.py

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI();
 @app.get("/")
 def home():
     return {"message": "Fast api with venv is running fine"}
+
+# params
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
