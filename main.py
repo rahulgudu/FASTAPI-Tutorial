063a8771c162fa26cabb93a4dad20438d5c37c19
Sushant Kumar Bishoi <117711778+rahulgudu@users.noreply.github.com>
2026-07-15 09:03:09 +0000

    response model and hide sensitive data

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import List, Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI();
 
@@ -24,3 +27,38 @@ def get_products(price: float = 20):
 @app.post("/items")
 def create_item(item: dict):
     return {"message": "Item created", "item": item}
+
+
+# Hiding sensitve information and response models
+class DocumentCreate(BaseModel):
+    title: str
+    content: str
+    confidential_notes: str
+    author_id: int
+    system_flags: List[str] = ["internal-draft"]
+
+
+class DocumentResponse(BaseModel):
+    title: str
+    content: str
+
+
+DOCUMENTS_DB = [
+    {
+        "title": "Q3 Financial Strategy",
+        "content": "The company targets a 15% revenue growth in Q3.",
+        "confidential_notes": "CEO considering downsizing the marketing team if targets fail.",
+        "author_id": 9921,
+        "system_flags": ["restricted", "highly-sensitive"]
+    }
+]
+
+# secure endpoints
+@app.get("/documents/public", response_model=List[DocumentResponse])
+async def get_public_documents():
+    return DOCUMENTS_DB
+
+#privileged endpoints
+@app.get("/documents/internal")
+async def get_internal_documents():
+    return DOCUMENTS_DB
